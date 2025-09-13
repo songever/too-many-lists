1049f6c4268acd7fdd1056a1ad8cd305c7dce32f
songever <1021940593@qq.com>
2025-09-13 13:20:52 +0800

    A Bad Link finished

--- src/first.rs
+++ src/first.rs
@@ -1,11 +1,86 @@
+use std::mem;
+
+#[derive(Clone)]
 struct Node {
     elem: i32,
     next: Link,
 }
+#[derive(Clone)]
 enum Link {
     Empty,
     More(Box<Node>)
 }
 pub struct List {
     head: Link,
+}
+
+impl List {
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node{
+            elem: elem,
+            next: std::mem::replace(&mut self.head, Link::Empty),
+        });
+
+        self.head = Link::More(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match std::mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
+    }
+}
+
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+        
+        while let Link::More(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
+            // boxed_node 在这里超出作用域并被 drop,
+            // 由于它的 `next` 字段拥有的 `Node` 被设置为 Link::Empty,
+            // 因此这里并不会有无边界的递归发生
+        }
+    }
+}
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        assert_eq!(list.pop(),Some(3));
+        assert_eq!(list.pop(),Some(2));
+
+        list.push(4);
+        list.push(5);
+        assert_eq!(list.pop(),Some(5));
+        assert_eq!(list.pop(),Some(4));
+        assert_eq!(list.pop(),Some(1));
+        assert_eq!(list.pop(),None);
+    }
+
+    #[test]
+    fn long_list() {
+        let mut list = List::new();
+        for i in 0..100000 {
+            list.push(i);
+        }
+        drop(list);
+    }
 }
